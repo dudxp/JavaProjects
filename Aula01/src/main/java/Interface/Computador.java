45216f0f2b2d58f20e06863a33485400ec4b8180
Eduardo Rossetti Boeira <boeied@2020spaces.com>
2022-03-21 17:29:41 -0300

    Diversos

--- Aula01/src/main/java/Interface/Computador.java
+++ Aula01/src/main/java/Interface/Computador.java
@@ -1,9 +1,21 @@
 package Interface;
 
-public class Computador extends Eletronico {
+public class Computador extends Eletronico implements Identificador {
+    private final String senha = "Qwerty90";
+    
     @Override
     public double calcularEnergia()
     {
         return super.getEnergiaGasta() * 3.10;
+    }
+    
+    @Override
+    public boolean identificar(String senha)
+    {
+        if (this.senha == senha)
+        {
+            return true;
+        }
+        return false;
     }
 }
---
+++ Aula01/src/main/java/Interface/Identificador.java
@@ -0,0 +1,5 @@
+package Interface;
+
+public interface Identificador {
+    boolean identificar(String senha);
+}
